ca47319ae92b8521a7729355a1b858518e149103
brewcold <seung.yoon.yu@gmail.com>
2024-05-17 19:24:35 +0900

    텍스트 컴포넌트 이름 변경

---
+++ src/_libs/components/_common/Txt/Txt.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import { ElementType, PropsWithChildren } from 'react';
+
+interface TextProps<T extends ElementType> extends PropsWithChildren {
+  as?: T | 'Link';
+  href?: 'string';
+  styleVariant?: string;
+}
+export function Txt<T extends ElementType>({ as, href, children, styleVariant, ...props }: TextProps<T>) {
+  const Component = as || 'span';
+  switch (Component) {
+    default:
+      return (
+        <Component className={styleVariant} {...props}>
+          {children}
+        </Component>
+      );
+    case 'Link':
+      return (
+        <Link href={href || ''} className={styleVariant} {...props}>
+          {children}
+        </Link>
+      );
+  }
+}
