2beee84e06ca7d079fa27d2ce2d366f6df565458
brewcold <seung.yoon.yu@gmail.com>
2024-05-18 17:06:57 +0900

    UI 텍스트 일부 분리

---
+++ src/_libs/_constants/alerts.ts
@@ -0,0 +1,4 @@
+export default {
+  IS_DUPLICATED: '이미 공대에 합류한 캐릭터입니다.',
+  IS_FULL: '공대원이 가득 찼습니다.',
+};
---
+++ src/_libs/_constants/meta.ts
@@ -0,0 +1,4 @@
+export default {
+  TITLE: '로스트아크 공대 최적화',
+  DESCRIPTION: '로스트아크 공대 최적화 도우미입니다.',
+};
