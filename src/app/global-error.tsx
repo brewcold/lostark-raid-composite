337814ab273dbe7dd691a88a472da21cb3c9abde
brewcold <seung.yoon.yu@gmail.com>
2024-05-24 18:27:06 +0900

    클라이언트 에러 핸들링 페이지 추가

---
+++ src/app/global-error.tsx
@@ -0,0 +1,30 @@
+'use client';
+import * as Sentry from '@sentry/nextjs';
+import NextError from 'next/error';
+import { useEffect, useState } from 'react';
+import { Btn } from 'src/_libs/components/_common/Btn/Btn';
+import Flex from 'src/_libs/components/_common/Flex/Flex';
+import { Spacing } from 'src/_libs/components/_common/Spacing/spacing';
+import { Txt } from 'src/_libs/components/_common/Txt/Txt';
+
+export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <html>
+      <body>
+        <div>
+          <Flex justifyContents="center" alignItems="center">
+            <Txt as="h1">오류가 발생했습니다.</Txt>
+            <Spacing size="2rem" />
+            <Txt as="Link" href="/">
+              돌아가기
+            </Txt>
+          </Flex>
+        </div>
+      </body>
+    </html>
+  );
+}
