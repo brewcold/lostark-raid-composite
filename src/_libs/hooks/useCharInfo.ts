7b52eb4a7371740f591702cc6ab2ef9d6d8c95d8
brewcold <seung.yoon.yu@gmail.com>
2024-05-27 15:53:27 +0900

    로스트아크 API 데이터를 클라이언트 사이드에서 fetch하도록 변경

--- src/_libs/hooks/useCharInfo.ts
+++ src/_libs/hooks/useCharInfo.ts
@@ -1,13 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
+import env from 'src/env';
 import { Armory } from '../types';
 import { http } from '../util/fetch';
 
 export const useCharInfo = (characterName: string) => {
   const name = characterName.trim();
-  const url = `/api/get_character?nickname=${name}`;
+  const auth = new Headers({ Authorization: `bearer ${env.larkKey}`, accept: 'application/json' });
+
+  const url = `https://developer-lostark.game.onstove.com/armories/characters/${characterName}`;
   return useQuery<Armory>({
     queryKey: ['info', name],
-    queryFn: async () => await http.GET(url),
+    queryFn: async () => await http.GET<Armory>(url, auth),
     enabled: !!name.length,
   });
 };
