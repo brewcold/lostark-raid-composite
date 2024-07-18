0b70177cd14b4a6612128c8231b1943b799d867e
brewcold <seung.yoon.yu@gmail.com>
2024-07-18 17:00:14 +0900

    텍스트 컴포넌트 리팩토링

--- src/_libs/components/_common/Txt/Txt.tsx
+++ src/_libs/components/_common/Txt/Txt.tsx
@@ -1,26 +1,28 @@
 import Link from 'next/link';
-import { ComponentProps, ElementType, PropsWithChildren } from 'react';
+import { ComponentPropsWithoutRef, ElementType, PropsWithChildren } from 'react';
 
 type TextProps<T extends ElementType> = {
   as?: T | 'Link';
   href?: string;
   styleVariant?: string;
-} & PropsWithChildren<ComponentProps<T>>;
+} & PropsWithChildren<ComponentPropsWithoutRef<T>>;
 
 export function Txt<T extends ElementType>({ as, href, children, styleVariant, ...props }: TextProps<T>) {
   const Component = as || 'span';
-  switch (Component) {
-    default:
-      return (
-        <Component className={styleVariant} {...props}>
+
+  if (Component === 'Link') {
+    return (
+      <Link href={href || ''}>
+        <span className={styleVariant} {...props}>
           {children}
-        </Component>
-      );
-    case 'Link':
-      return (
-        <Link href={href || ''} className={styleVariant} {...props}>
-          {children}
-        </Link>
-      );
+        </span>
+      </Link>
+    );
   }
+
+  return (
+    <Component className={styleVariant} {...props}>
+      {children}
+    </Component>
+  );
 }
