887e57ea46a72f632a417a67e4212b9d347e9ae5
brewcold <seung.yoon.yu@gmail.com>
2024-09-18 16:49:30 +0900

    초월 계산 로직 개선

--- src/_libs/calc/trancendence.ts
+++ src/_libs/calc/trancendence.ts
@@ -1,6 +1,4 @@
-import ui from '../constants/ui';
-import { ArmoryEquipment, Tooltip } from '../types';
-import { isArmor, isWeapon } from './boolean/is-armor-parts';
+import { ArmoryEquipment } from '../types';
 
 export function calcTranscendenceGrade(tooltip: string): number {
   const match = tooltip.match(/\[초월\].*?(\d+)/);
@@ -8,22 +6,25 @@ export function calcTranscendenceGrade(tooltip: string): number {
 }
 
 export function calcTotalTranscendence(equipments: ArmoryEquipment[]): { 방어구: number; 무기: number } {
-  //현재 무기, 방어구 초월 합 구현을 위해서는 아이콘 파싱을 해야 하는 상황임
-  let armor: string = '';
-  let weapon: string = '';
+  let html: string = '';
+
   equipments.some(e => {
-    const temp = JSON.parse(e.Tooltip)?.Element_009?.value?.Element_000?.contentStr?.Element_001?.contentStr;
-    if (!temp) return false;
-    else {
-      armor = temp;
+    const tooltip = JSON.parse(e.Tooltip);
+    const equipment = tooltip.Element_009?.value?.Element_000?.contentStr?.Element_001?.contentStr;
+    const esther = tooltip.Element_010?.value?.Element_000?.contentStr?.Element_001?.contentStr;
+
+    if (typeof html === 'undefined') {
+      html = esther;
+      return true;
+    } else {
+      html = equipment;
       return true;
     }
   });
 
   const parser = new DOMParser();
-  const 방초 = parser.parseFromString(armor, 'text/html');
-  const 방어구 = Number(방초.body.textContent?.slice(13).slice(0, -1)) || 0;
-  const 무기 = 0;
+  const TEXT = parser.parseFromString(html, 'text/html');
+  const 초월수치 = Number(TEXT.body.textContent?.slice(13).slice(0, -1)) || 0;
 
-  return { 방어구, 무기 };
+  return { 방어구: 초월수치, 무기: 0 };
 }
